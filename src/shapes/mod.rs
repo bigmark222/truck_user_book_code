113209704eef59126a4a74629777d22c82b4a90e
bigmark222 <jonh9924@proton.me>
2025-11-27 17:44:56 -0600

    progress, through page 2.4.0

---
+++ src/shapes/mod.rs
@@ -0,0 +1,21 @@
+pub mod triangle;
+pub use triangle::triangle;
+
+pub mod square;
+pub use square::square;
+
+pub mod tetrahedron;
+pub use tetrahedron::tetrahedron;
+
+pub mod hexahedron;
+pub use hexahedron::hexahedron;
+
+pub mod octahedron;
+pub use octahedron::octahedron;
+
+pub mod dodecahedron;
+pub use dodecahedron::dodecahedron;
+
+pub mod icosahedron;
+pub use icosahedron::icosahedron;
+
